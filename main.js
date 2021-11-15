f7a86f2c17dc40951bc58ba764b1b4763732bd3a
csslick <digitone@naver.com>
2021-11-15 23:06:52 +0900

    pc layout

--- main.js
+++ main.js
@@ -1,70 +1,95 @@
 const data = [
-    {
-        id: 1,
-        name: '최수진',
-        img: `images/최수진.png`,
-        url: 'https://monari12.github.io/portfolio/',
-    },
-    {
-        id: 2,
-        name: '양상현',
-        img: `images/양상현.png`,
-        url: 'https://sangchu98.github.io/Portfolio/',
-    },
-    {
-        id: 3,
-        img: `https://via.placeholder.com/300/300&text=${3}`
-    },
-    {
-        id: 4,
-        img: `https://via.placeholder.com/300/300&text=${4}`
-    },
-    {
-        id: 5,
-        img: `https://via.placeholder.com/300/300&text=${5}`
-    },
-    {
-        id: 6,
-        img: `https://via.placeholder.com/300/300&text=${6}`
-    },
-    {
-        id: 7,
-        img: `https://via.placeholder.com/300/300&text=${7}`
-    },
-    {
-        id: 8,
-        img: `https://via.placeholder.com/300/300&text=${8}`
-    },
-    {
-        id: 9,
-        img: `https://via.placeholder.com/300/300&text=${9}`
-    },
-]
+  {
+    id: 1,
+    name: "권영우",
+    img: `https://via.placeholder.com/300?text=kwon-yeong-woo`,
+    url: "https://via.placeholder.com/300?text=1",
+  },
+  {
+    id: 2,
+    name: "김성헌",
+    img: "images/김성헌.png",
+    url: "https://trainingezen.github.io/Pv002/",
+  },
+  {
+    id: 3,
+    name: "김채은",
+    img: "images/김채은.png",
+    url: "https://chaeeun7131.github.io/portfolio/",
+  },
+  {
+    id: 4,
+    name: "박철현",
+    img: "images/박철현.png",
+    url: "https://ironpark1105.github.io/wepPortfolio/",
+  },
+  {
+    id: 5,
+    name: "양상현",
+    img: "images/양상현.png",
+    url: "https://sangchu98.github.io/Portfolio/",
+  },
+  {
+    id: 6,
+    name: "이지은",
+    img: "images/이지은.png",
+    url: "https://zinio3o.github.io/portfolio/",
+  },
+  {
+    id: 7,
+    name: "최수미",
+    img: `https://via.placeholder.com/300/eee?text=choi-su-mi`,
+    url: "https://via.placeholder.com/300/300&text=4",
+  },
+  {
+    id: 8,
+    name: "최수진",
+    img: `images/최수진.png`,
+    url: "https://monari12.github.io/portfolio/",
+  },
+  {
+    id: 9,
+    name: "황은빈",
+    img: "images/황은빈.png",
+    url: "https://vol2028.github.io/HEB-portpoilo/",
+  },
 
-const list = document.querySelector('#works .work-list');
+];
+
+const list = document.querySelector("#works .work-list");
 let html = ``;
 
-var img_arr = 0;
-for (var i = 0; i < 15; i++) {
-    // console.log(data[i], i);
-    if (i == 5 || i == 9 || i == 10 || i == 11 || i == 13 || i == 14) {
-        html += `
-            <li class="col-div-5">
-                
-            </li>            
-        `
-    } else{
-        html += `
-            <li class="col-div-5">
-                <a href="${data[img_arr].url}">
-                    <img src="${data[img_arr].img}" alt="${i}">
+for (var i = 0; i < data.length; i++) {
+  // console.log(data[i], i);
+  html += `
+            <li class="col-div-5 item">
+                <a href="#">
+                    <img src="${data[i].img}" alt="${i}">
                 </a>
             </li>
         `;
-        img_arr++;
-    }
 }
 
-
-
 list.innerHTML = html;
+
+
+$(function(){
+    $('.work-list > li').on('click', function(){
+        console.log($(this).index())
+        const idx = $(this).index();
+        const work_info = `
+            <div class="work-info">
+                <figure>
+                    <a href="${data[idx].url}" target="_blank">
+                        <img src="${data[idx].img}" alt="${idx}">
+                    </a>
+                    <figcaption>${data[idx].name}</figcaption>
+                </figure>          
+                <a class="btn btn-outline-primary" href="${data[idx].url}" target="_blank">작품보러가기</a>
+            </div>
+        `
+        $('#works .figure').html(work_info);
+    })
+
+    $('.work-list > li').eq(0).trigger('click');
+})
